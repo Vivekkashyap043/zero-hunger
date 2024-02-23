595c275400da8042f44b3e6bdebd78a2c2f39cf6
vivek <vivekkashyap043@gmail.com>
2024-02-23 21:07:18 +0530

    header

--- src/components/donar-dashboard/DonarDashboard.js
+++ src/components/donar-dashboard/DonarDashboard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Home() {
+function DonarDashboard() {
   return (
     <div>
-      Home
+      
     </div>
   )
 }
 
-export default Home
+export default DonarDashboard
