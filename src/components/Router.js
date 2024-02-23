47965b6cb5733c8e770b7a464bd702c236902824
Uday <Uday@gmail.com>
2024-02-23 23:31:41 +0530

    uday commit

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,19 +3,32 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './Root'
 import Home from './home/Home'
 import Contactus from './contactus/Contactus'
+import Login from './login/Login'
+import DonarLogin from './login/DonarLogin'
+import VolunteerLogin from './login/VolunteerLogin'
 
 function Router() {
     let router=createBrowserRouter([
         {
             path:"/",
             element:<Root/>,
-            children:[
+            children:[  
                 {
-                    path:" ",
-                    element:<Home />
+                    path:"",
+                    element:<Login/>,
+                    children:[
+                      {
+                        path:"/donar-login",
+                        element:<DonarLogin/>
+                      },
+                      {
+                        path:"/volunteer-login",
+                        element:<VolunteerLogin/>
+                      }
+                    ]
                 },
                 {
-                    path:"contactus",
+                    path:"/contactus",
                     element:<Contactus/>
                 }
             ]
